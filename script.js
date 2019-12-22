52e7186deda84e1d7d1dc6ebffcf7ea498382c86
Catherine Pedersen <cathy.pedersen@gmail.com>
2019-12-22 17:01:29 -0500

    Fixed burger menu; added media queries

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* Toggle between showing and hiding the navigation menu links when the 
 user clicks on the hamburger menu bar icon */
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-      x.style.display = "block";
+      x.className = "topNav";
     }
 }
